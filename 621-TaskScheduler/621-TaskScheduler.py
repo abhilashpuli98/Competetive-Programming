c122bd6d6bc820dd32ad41de590a81798bde40d2
Abhilash Puli <abhilashpuli.dev@gmail.com>
2026-05-15 23:15:36 +0530

    Create 621-TaskScheduler

---
+++ 621-TaskScheduler/621-TaskScheduler.py
@@ -0,0 +1,9 @@
+# Last Updated: 5/15/2026, 11:15:36 PM
+class Solution(object):
+    def leastInterval(self, tasks, n):
+        count = {}
+        for t in tasks:
+            count[t] = count.get(t, 0) + 1
+        f_max = max(count.values())
+        max_count = sum(1 for v in count.values() if v == f_max)
+        return max(len(tasks), (f_max - 1) * (n + 1) + max_count)
