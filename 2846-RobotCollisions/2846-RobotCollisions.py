c53abcdd3ac871fad11104f6fd13f1b91385c695
Abhilash Puli <abhilashpuli.dev@gmail.com>
2026-05-15 23:11:05 +0530

    Create 2846-RobotCollisions

---
+++ 2846-RobotCollisions/2846-RobotCollisions.py
@@ -0,0 +1,38 @@
+# Last Updated: 5/15/2026, 11:11:05 PM
+class Solution:
+    def survivedRobotsHealths(self, positions, healths, directions):
+
+        n = len(positions)
+        order = sorted(range(n), key=lambda i: positions[i])
+
+        h = healths[:]
+        alive = [True]*n
+        stack = []
+
+        for idx in order:
+
+            if directions[idx] == 'R':
+                stack.append(idx)
+
+            else:
+                while stack:
+
+                    top = stack[-1]
+
+                    if h[top] < h[idx]:
+                        alive[top] = False
+                        stack.pop()
+                        h[idx] -= 1
+
+                    elif h[top] > h[idx]:
+                        alive[idx] = False
+                        h[top] -= 1
+                        break
+
+                    else:
+                        alive[top] = False
+                        alive[idx] = False
+                        stack.pop()
+                        break
+
+        return [h[i] for i in range(n) if alive[i]]
