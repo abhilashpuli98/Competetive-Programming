d2f280842557bb3d381e4815685f4214fdd82092
Abhilash Puli <abhilashpuli.dev@gmail.com>
2026-05-15 23:14:02 +0530

    Create 1159-SmallestSubsequenceOfDistinctCharacters

---
+++ 1159-SmallestSubsequenceOfDistinctCharacters/1159-SmallestSubsequenceOfDistinctCharacters.py
@@ -0,0 +1,18 @@
+# Last Updated: 5/15/2026, 11:14:02 PM
+class Solution:
+    def smallestSubsequence(self, s: str) -> str:
+        stack=[]
+        seen=dict()
+        freq=dict()
+        for char in s:
+            freq[char]=freq.get(char,0)+1
+        for char in s:
+            freq[char]-=1
+            if seen.get(char,False):
+                continue
+            while stack and stack[-1]>char and freq[stack[-1]]>0:
+                seen[stack[-1]]=False
+                stack.pop()
+            stack.append(char)
+            seen[char]=True
+        return ''.join(stack)
