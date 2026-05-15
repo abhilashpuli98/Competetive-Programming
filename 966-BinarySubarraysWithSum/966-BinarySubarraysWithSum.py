7c1eaf1c7233e076801466ad46db7b62d8b11a10
Abhilash Puli <abhilashpuli.dev@gmail.com>
2026-05-15 23:14:37 +0530

    Create 966-BinarySubarraysWithSum

---
+++ 966-BinarySubarraysWithSum/966-BinarySubarraysWithSum.py
@@ -0,0 +1,14 @@
+# Last Updated: 5/15/2026, 11:14:36 PM
+class Solution:
+    def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
+        return self.atMost(nums, goal)- self.atMost(nums, goal-1)
+
+    def atMost(self, nums: List[int], goal: int) -> int:
+        head, tail, total, result = 0, 0, 0, 0
+        for head in range(len(nums)):
+            total += nums[head]
+            while total > goal and tail <= head:
+                total -= nums[tail]
+                tail += 1
+            result += head - tail + 1
+        return result
