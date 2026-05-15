b96c538e69c27f1d2c160a5ec75ddbe52dfaa493
Abhilash Puli <abhilashpuli.dev@gmail.com>
2026-05-15 23:13:04 +0530

    Create 1646-KthMissingPositiveNumber

---
+++ 1646-KthMissingPositiveNumber/1646-KthMissingPositiveNumber.py
@@ -0,0 +1,19 @@
+# Last Updated: 5/15/2026, 11:13:04 PM
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        # for i in range(len(arr)):
+        #     if arr[i]<=k:
+        #         k+=1
+        #     else:
+        #         break
+        # return k
+        low=0
+        high=len(arr)-1
+        while low<=high:
+            mid=(low+high)//2
+            missing=(arr[mid])-(mid+1)
+            if missing < k:
+                low=mid+1
+            else:
+                high=mid-1
+        return low+k
