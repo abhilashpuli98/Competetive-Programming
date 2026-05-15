46f00a434328a764eb947a8b32e10999f1802d3b
Abhilash Puli <abhilashpuli.dev@gmail.com>
2026-05-15 23:11:17 +0530

    Create 2732-Counter

---
+++ 2732-Counter/2732-Counter.js
@@ -0,0 +1,18 @@
+// Last Updated: 5/15/2026, 11:11:17 PM
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    
+    return function() {
+        return n++;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
