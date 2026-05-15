dabbe4cc4b068a0442857699fd263ef8a48a9747
Abhilash Puli <abhilashpuli.dev@gmail.com>
2026-05-15 23:09:17 +0530

    Create 4176-MinimumKToReduceArrayWithinLimit

---
+++ 4176-MinimumKToReduceArrayWithinLimit/4176-MinimumKToReduceArrayWithinLimit.py
@@ -0,0 +1,15 @@
+# Last Updated: 5/15/2026, 11:09:17 PM
+class Solution:
+    def minimumK(self, nums: List[int]) -> int:
+        def ops(k):
+            return sum((x+k-1)//k for x in nums)
+        l,r=1,max(nums)+len(nums)
+        ans=r
+        while l<=r:
+            mid=(l+r)//2
+            if ops(mid)<=mid*mid:
+                ans=mid
+                r=mid-1
+            else:
+                l=mid+1
+        return ans
