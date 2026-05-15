ddfbc6c1de1a25543e4c8c7d55d4c531e2457400
Abhilash Puli <abhilashpuli.dev@gmail.com>
2026-05-15 23:14:22 +0530

    Create 1034-SubarraysWithKDifferentIntegers

---
+++ 1034-SubarraysWithKDifferentIntegers/1034-SubarraysWithKDifferentIntegers.py
@@ -0,0 +1,17 @@
+# Last Updated: 5/15/2026, 11:14:22 PM
+class Solution:
+    def subarraysWithKDistinct(self, A, K):
+        return self.atMostK(A, K) - self.atMostK(A, K - 1)
+
+    def atMostK(self, A, K):
+        count = collections.Counter()
+        res = i = 0
+        for j in range(len(A)):
+            if count[A[j]] == 0: K -= 1
+            count[A[j]] += 1
+            while K < 0:
+                count[A[i]] -= 1
+                if count[A[i]] == 0: K += 1
+                i += 1
+            res += j - i + 1
+        return res
