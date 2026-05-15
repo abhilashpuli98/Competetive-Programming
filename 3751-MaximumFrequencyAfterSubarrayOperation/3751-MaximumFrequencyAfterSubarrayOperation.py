82618095d689c05b0684004634fa9422b86ebf3b
Abhilash Puli <abhilashpuli.dev@gmail.com>
2026-05-15 23:10:01 +0530

    Create 3751-MaximumFrequencyAfterSubarrayOperation

---
+++ 3751-MaximumFrequencyAfterSubarrayOperation/3751-MaximumFrequencyAfterSubarrayOperation.py
@@ -0,0 +1,17 @@
+# Last Updated: 5/15/2026, 11:10:01 PM
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        freq = Counter(nums)
+        def kadane(x):
+            res=cur=0
+            for y in nums:
+                if x == y:
+                    cur+=1
+                if y == k:
+                    cur-=1
+                if cur<0:
+                    cur=0
+                res = max(res,cur)
+            return res
+        result = max(kadane(x) for x in freq)
+        return freq[k]+result
