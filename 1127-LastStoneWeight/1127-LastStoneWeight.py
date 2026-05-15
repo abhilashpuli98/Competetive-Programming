5d7bd57a55879b4ce5ad086f269f8302ab91b94b
Abhilash Puli <abhilashpuli.dev@gmail.com>
2026-05-15 23:14:04 +0530

    Create 1127-LastStoneWeight

---
+++ 1127-LastStoneWeight/1127-LastStoneWeight.py
@@ -0,0 +1,15 @@
+# Last Updated: 5/15/2026, 11:14:04 PM
+class Solution(object):
+    def lastStoneWeight(self, stones):
+        stones=[-i for i in stones];
+        heapq.heapify(stones);
+        while(len(stones)>1):
+            y=-heapq.heappop(stones);
+            x=-heapq.heappop(stones);
+            heapq.heappush(stones,x-y);
+        return -heapq.heappop(stones)
+        """
+        :type stones: List[int]
+        :rtype: int
+        """
+        
