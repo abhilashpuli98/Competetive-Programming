7e247cc99620e3ce2bd4c4b05c177181b2f2facd
Abhilash Puli <abhilashpuli.dev@gmail.com>
2026-05-15 23:09:26 +0530

    Create 4130-MaximumScoreAfterBinarySwaps

---
+++ 4130-MaximumScoreAfterBinarySwaps/4130-MaximumScoreAfterBinarySwaps.py
@@ -0,0 +1,22 @@
+# Last Updated: 5/15/2026, 11:09:26 PM
+class Solution(object):
+    def maximumScore(self, nums, s):
+        """
+        :type nums: List[int]
+        :type s: str
+        :rtype: int
+        """
+        banterisol = (nums, s)
+
+        heap = []
+        score = 0
+        ones_used = 0
+
+        for i in range(len(nums)):
+            heapq.heappush(heap, -nums[i])
+
+            if s[i] == '1':
+                score += -heapq.heappop(heap)
+                ones_used += 1
+
+        return score
