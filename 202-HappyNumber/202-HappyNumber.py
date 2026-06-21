726403b6f2b6faa48e1f48148cda118e650af4d9
Abhilash Puli <abhilashpuli.dev@gmail.com>
2026-06-22 00:42:37 +0530

    Create 202-HappyNumber

---
+++ 202-HappyNumber/202-HappyNumber.py
@@ -0,0 +1,14 @@
+# Last Updated: 6/22/2026, 12:42:38 AM
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen=set()
+        if n==1:
+            return True
+        while n!=1:
+            n=sum([int(digit)**2 for digit in str(n)])
+            if n in seen:
+                return False
+            seen.add(n)
+        return True
+
+        
