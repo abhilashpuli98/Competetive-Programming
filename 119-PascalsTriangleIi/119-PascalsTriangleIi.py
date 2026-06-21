208a6231020fc8ad25811d04e3a12bcfd660a776
Abhilash Puli <abhilashpuli.dev@gmail.com>
2026-06-22 00:43:47 +0530

    Create 119-PascalsTriangleIi

---
+++ 119-PascalsTriangleIi/119-PascalsTriangleIi.py
@@ -0,0 +1,13 @@
+# Last Updated: 6/22/2026, 12:43:49 AM
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+        if rowIndex==0:
+            return [1]
+        prev=[1]
+        for i in range(1,rowIndex+1):
+            curr=[1]
+            for j in range(1,len(prev)):
+                curr.append(prev[j-1]+prev[j])
+            curr.append(1)
+            prev=curr
+        return prev
