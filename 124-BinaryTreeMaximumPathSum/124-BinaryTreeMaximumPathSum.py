92e9e0fe2e9958ec1ab8795b22f439cc59865a69
Abhilash Puli <abhilashpuli.dev@gmail.com>
2026-06-22 00:43:42 +0530

    Create 124-BinaryTreeMaximumPathSum

---
+++ 124-BinaryTreeMaximumPathSum/124-BinaryTreeMaximumPathSum.py
@@ -0,0 +1,22 @@
+# Last Updated: 6/22/2026, 12:43:44 AM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        res=root.val 
+        def dfs(root):
+            nonlocal res
+            if not root:
+                return 0
+            left=max(0,dfs(root.left))
+            right=max(0,dfs(root.right))
+            res=max(res,left+right+root.val)
+            return max(left,right)+root.val
+        dfs(root)
+        return res
+
+        
