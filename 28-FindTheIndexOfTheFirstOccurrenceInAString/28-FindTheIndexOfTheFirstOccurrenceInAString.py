25805896508f6dc78c70f4290649c92ea624abb0
Abhilash Puli <abhilashpuli.dev@gmail.com>
2026-06-22 00:45:07 +0530

    Create 28-FindTheIndexOfTheFirstOccurrenceInAString

---
+++ 28-FindTheIndexOfTheFirstOccurrenceInAString/28-FindTheIndexOfTheFirstOccurrenceInAString.py
@@ -0,0 +1,7 @@
+# Last Updated: 6/22/2026, 12:45:09 AM
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        for i in range(len(haystack)-len(needle)+1):
+            if haystack[i:i+len(needle)]==needle:
+                return i
+        return -1
