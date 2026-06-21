323cbf3fdbac07f44543ffafa70831352f01f970
Abhilash Puli <abhilashpuli.dev@gmail.com>
2026-06-22 00:42:02 +0530

    Create 235-LowestCommonAncestorOfABinarySearchTree

---
+++ 235-LowestCommonAncestorOfABinarySearchTree/235-LowestCommonAncestorOfABinarySearchTree.py
@@ -0,0 +1,17 @@
+# Last Updated: 6/22/2026, 12:42:03 AM
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        while root:
+            if max(p.val,q.val)<root.val:
+                root=root.left
+            elif min(p.val,q.val)>root.val:
+                root=root.right
+            else:
+                return root
